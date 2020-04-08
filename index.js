1d2c15f44bf4bdda0110cbf63435a076f9041ff4
Alan Marcos <alan.marcos@credpago.com>
2020-04-08 17:57:14 -0300

    Exporta Title e Button

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { version } = require('./package.json');
+import Title from './src/public/Title';
+import Button from './src/public/Button';
 
 const CPUI = {
-  version
+  version,
+  Title,
+  Button
 }
 
 module.exports = CPUI;
