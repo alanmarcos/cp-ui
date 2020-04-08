3d48312f5c7e92cfadf1ed841c5b5c278f3dc247
Alan Marcos <alan.marcos@credpago.com>
2020-04-08 15:58:35 -0300

    Adiciona readme ao primeiro Storie

--- src/internals/welcome/index.js
+++ src/internals/welcome/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import LogoIcon from '../assets/images/logo-icon.png';
 import LogoText from '../assets/images/logo-text.png';
 import CSS from './index.module.scss';
-const Package = require('../../../package.json');
+import README from '../../../README.md';
+import Package from '../../../package.json';
+const showdown  = require('showdown');
+
 
 const Welcome = () => {
 
   const { version } = Package;
+  const converter = new showdown.Converter();
 
   return (
     <div className={CSS.welcomeWrapper}>
@@ -16,6 +20,8 @@ const Welcome = () => {
         <small className={CSS.versionNumber}>Versão: {version}</small>
 
         <p>Guia de uso de marca e Biblioteca de componentes front-end feitos em React</p>
+
+        <div dangerouslySetInnerHTML={{ __html: converter.makeHtml(README) }}></div>
       </div>
     </div>
   )
