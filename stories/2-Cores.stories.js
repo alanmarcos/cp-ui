35b149de7c5d14822e8dbad483bc19721fbc4a02
Alan Marcos <alan.marcos@credpago.com>
2020-04-08 14:34:52 -0300

    Adiciona cores

--- stories/2-Cores.stories.js
+++ stories/2-Cores.stories.js
@@ -1,7 +1,21 @@
 import React from 'react';
-export const Cores = () => <div>lista de cores vai aqui</div>;
+import Cores from '../src/internals/colors/index';
+import { withInfo } from '@storybook/addon-info';
 
+export const Todas = () => <Cores />;
 export default {
-  title: 'Todas as cores',
-  component: Cores,
+  title: 'Cores',
+  component: Todas,
+  decorators: [withInfo],
+  parameters: {
+    info: { 
+      inline: true,
+      text: 'Todas as cores oficiais da CredPago em hexadecimal. Para a versão em CMYK, é melhor chamar o marketing.',
+      header: true,
+      source: false,
+      propTypes: false,
+      propTables: false,
+    },
+  },
 };
+
